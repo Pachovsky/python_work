82760417cc98fb0aa3d0adddd1e52b678f4b7c85
Pachovsky <48571965+Pachovsky@users.noreply.github.com>
2021-05-27 23:27:15 +0200

    Update greeter.py

--- greeter.py
+++ greeter.py
@@ -4,7 +4,7 @@ name = input("Please enter your name: ")
 print("Hello, " + name + "!")
 
 # storing your prompt in a variable and pass that variable to the input() function:
-prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt = "If you tell me who you are, I can personalize the messages you see."
 prompt += "\nWhat is your first name? "
 
 name = input(prompt)
@@ -12,4 +12,37 @@ print("\nHello, " + name + "!")
 
 # Using int() to Accept Numerical Input
 age = input("How old are you? ")
-print(age)
+age = int(age)
+if age >= 18:
+    print("You are 18+! Your age is: " + str(age))
+
+# rollercoaster
+height = input("How tall are you in centimeters? ")
+height = int(height)
+
+if height >= 120:
+    print("\nYou are tall enough to ride the rollercoaster!")
+else:
+    print("\nYou'll be able to ride when you are a little older.")
+
+# the modulo operator
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+if number % 2 == 0:
+    print("\nThe number " + str(number) + " is even.")
+else:
+    print("\nThe number " + str(number) + " is odd.")
+
+# 7-1 rental car
+car = input("What kind of rental car would you like? ")
+print("\nLet me see if I can find a " + car + " for you!")
+
+print("\n")
+# 7-2 restaurant seating
+seats = input("How many people are in your dinner group? ")
+seats = int(seats)
+
+if seats > 8:
+    print("You'll have to wait for a table.")
+else:
+    print("Your table is ready.")
