398c92fc1d1dd85e5686cf16c4b8c0d86f591374
Pachovsky <48571965+Pachovsky@users.noreply.github.com>
2021-05-31 23:12:09 +0200

    Update printing_models.py

---
+++ printing_models.py
@@ -0,0 +1,22 @@
+# Modifying a List in a Function
+
+# start with some designs that need to be printed.
+unprinted_designs = ["iphone case", "robot pendant", "dodecahedron"]
+completed_models = []
+
+# simulate printing each design, until none are left.
+# move each design to completed_models after printing.
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+
+    # simulate creating a 3D print from the design.
+    print("Printing model: " + current_design)
+    completed_models.append(current_design)
+
+# display all completed models.
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
+
+# unprinted_designs is now empty, everything has been moved to completed_models:
+print(unprinted_designs)
