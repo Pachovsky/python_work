b6d8d663f082b6fd4186ec8faa7f736c2de7e5dd
Pachovsky <48571965+Pachovsky@users.noreply.github.com>
2021-06-02 21:10:17 +0200

    Update greet_users.py

--- greet_users.py
+++ greet_users.py
@@ -3,8 +3,8 @@
 def greet_users(names):
     """Print a simple greeting to each user in the list."""
     for name in names:
-        msg = "Hello, " + name.title() + "!"
-        print(msg)
+        message = "Hello, " + name.title() + "!"
+        print(message)
 
 usernames = ["hannah", "ty", "margot"]
 greet_users(usernames)
